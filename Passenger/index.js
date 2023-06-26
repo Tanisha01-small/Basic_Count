d384026f1cbabbf61b91e9f1b9f975efe188a6b3
Tanisha <tanishasinghal0101@gmail.com>
2023-06-27 04:00:37 +0530

    Ready

--- Passenger/index.js
+++ Passenger/index.js
@@ -1,1 +1,22 @@
-document.getElementById("c").innerText=7
+
+let countEl= document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+console.log(countEl);
+let count=0;
+
+function increment() {
+    //body of the fucntion for execution 
+    console.log("clicked");
+    count+=1;
+    countEl.textContent=count;
+    console.log(count);
+}
+
+function save(){
+    let newSave=" "+count + " - ";
+    saveEl.textContent+=newSave;
+    console.log(newSave);
+    countEl.textContent=0;
+    count=0;
+}
+
